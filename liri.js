daa91bb1acf64e39881c82b2060842f25b84a961
Barbara Whiting <whitingba@gmail.com>
2019-08-25 07:39:15 -0600

    corrected the files, I should now have all the required files for the assignment

---
+++ liri.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+
+var keys = require("./keys.js");
+
+// * You should then be able to access your keys information like so
+
+//   ```js
+//   var spotify = new Spotify(keys.spotify);
+//   ```
+
+
+// 9. Make it so liri.js can take in one of the following commands:
+
+//    * `concert-this`
+
+//    * `spotify-this-song`
+
+//    * `movie-this`
+
+//    * `do-what-it-says`
+
+
+// 1. `node liri.js concert-this <artist/band name here>`
+
+//    * This will search the Bands in Town Artist Events API (`"https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"`) for an artist and render the following information about each event to the terminal:
+
+//      * Name of the venue
+
+//      * Venue location
+
+//      * Date of the Event (use moment to format this as "MM/DD/YYYY")
