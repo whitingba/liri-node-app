d60a84ea4cea34130861d845243d8e4bab1f97dd
Barbara Whiting <whitingba@gmail.com>
2019-09-01 11:26:43 -0600

    final working assignment

--- liri.js
+++ liri.js
@@ -1,5 +1,7 @@
+//require the .env file that holds my unique spotify indentifiers
 require('dotenv').config();
 
+//require the key.js file 
 let keys = require('./keys.js');
 
 //require the axios package
@@ -11,12 +13,19 @@ let moment = require('moment');
 //require the node-spotify-api
 let Spotify = require('node-spotify-api');
 
+//node package for reading or writing a txt file
+let fs = require('fs');
+
+//variable created for the command line arguments in index 2
 let commandName = process.argv[2];
 
+//variable created for the command line arguments in index 3 which will take strings and join them together for searching the APIs
 let inputData = process.argv[3] ? process.argv.slice(3).join(" ") : "";
+//Both of the last two variables will be called in my userChoice function below and will be my arguments passed in
 
 
-
+//switch case statement  created with parameters that refer to the cases (such as concert-this or movie-this) as well as a parameter that refers back to the data in my individual
+//functions created for each thing I want LIRI to do.
 let userChoice = function (doThisChoice, data) {
 
     switch (doThisChoice) {
@@ -40,7 +49,7 @@ let userChoice = function (doThisChoice, data) {
 
 
 
-
+//**********************************CONCERT-THIS**********************************************/
 
 //create a function to get the Bandsintown data from the bandsintown api, passing in a paramater of chodenArtist that is plugged into the bandsintown url to search for the chosen artist.
 let concertInfo = function (artist) {
@@ -77,6 +86,7 @@ let concertInfo = function (artist) {
 }
 
 
+//**********************************SPOTIFY-THIS**********************************************/
 let spotify = new Spotify(keys.spotify);
 
 //Function to grabArtist name from Spotify API data
@@ -110,7 +120,7 @@ let spotifySong = function (songName) {
                 );
             }
             //Final console log that displays the total number of songs that were found in the search
-            console.log(songs.length + " songs found.")
+            console.log(songs.length + ' songs found')
         }
     );
 
@@ -121,64 +131,49 @@ let spotifySong = function (songName) {
 
 
 
-//********************************************************************************************* */
+//**********************************MOVIE-THIS**********************************************/
 
 
-// 3. `node liri.js movie-this '<movie name here>'`
-
-//    * This will output the following information to your terminal/bash window:
-
-//      ```
-//        * Title of the movie.
-//        * Year the movie came out.
-//        * IMDB Rating of the movie.
-//        * Rotten Tomatoes Rating of the movie.
-//        * Country where the movie was produced.
-//        * Language of the movie.
-//        * Plot of the movie.
-//        * Actors in the movie.
-//      ```
-
-//    * If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
-
-
-//    * You'll use the `axios` package to retrieve data from the OMDB API. Like all of the in-class activities, the OMDB API requires an API key. You may use `trilogy`.
-//****************************************************************************** */
-
-
-
-//FIXME: - need to correct error then call Mr. Nobody movie and create a function for this to assign to movie-this
-
+//function crated to pull in the movie data from omdb
 let movieInfo = function (movie) {
 
-
+    //if a movie title is not input then the default movie is chosen for the user
     let chosenMovie = (movie == "") ? "Mr Nobody" : movie;
-    console.log('movie:' + JSON.stringify(chosenMovie));
-
+    //console.log('movie:' + JSON.stringify(chosenMovie));
+    //URL that will be queried
     let URL = "http://www.omdbapi.com/?t=" + chosenMovie + "&y=&plot=short&apikey=trilogy";
-
-
     axios.get(URL).then(function (response) {
 
-
+        //console log the results from the query
         console.log(
             `------------------------------ \nMovie Title: ${response.data.Title} \nYear of Release: ${response.data.Year} \nIMDB Rating: ${response.data.imdbRating} \nRotten Tomatoes rating: ${response.data.Ratings[1].Value} \nProduction Country: ${response.data.Country} \nLanguage: ${response.data.Language} \nActors: ${response.data.Actors} \nPlot: ${response.data.Plot} \n------------------------------`
         );
-
 
     })
 };
 
 
-//TODO:
+//**********************************DO-WHAT-IT-SAYS**********************************************/
 
-// 4. `node liri.js do-what-it-says`
+let doWhatItSays = function () {
+    //code to read the random.txt file
+    fs.readFile('random.txt', 'utf8', function (error, data) {
 
-//    * Using the `fs` Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
 
-//      * It should run `spotify-this-song` for "I Want it That Way," as follows the text in `random.txt`.
 
-//      * Edit the text in random.txt to test out the feature for movie-this and concert-this.
+        console.log(data);
+
+        //variable created to grab the data from the .txt file and put it into an array
+        let dataArr = data.split(',');
+
+        //ternary statement to check if the length of the array is 2 then run the userChoice function with the first two indexes in the array otherwise it will only run with the first index which 
+        //will pull the default song set
+        (dataArr.length === 2) ? userChoice(dataArr[0], dataArr[1]) : userChoice(dataArr[0]);
+
+
+    })
+
+}
 
 
 
